8e822f4434f31759e6d47a7067ca804557687656
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-02-11 16:24:03 -0300

    fix(gatsby-config.js) Fix SEO data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Multei!`,
+    description: `Denuncie quem para seu carro em vagas para pessoas com deficiência`,
+    author: `@multeiapp`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#F6F4F3`,
+        theme_color: `#0069AE`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
