e100538d72b2b58d52a0df6d419db172432ef122
Adalberto <adalberto.maia@gmail.com>
2020-02-14 18:19:54 -0300

    test(index.test.js) fix broken test

--- src/components/Header/index.test.js
+++ src/components/Header/index.test.js
@@ -1,6 +1,6 @@
 import React from "react"
 import renderer from "react-test-renderer"
-import Header from "../header"
+import Header from "."
 describe("Header", () => {
   it("renders correctly", () => {
     const tree = renderer
