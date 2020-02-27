c82e458c41ed8903a965436823550f24320fff10
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-02-26 23:33:52 -0300

    chore(Button) remove old button component

--- src/components/Button/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import './index.css'
-
-const Button = ({color, ...otherProps}) => (
-  <button className={`button ${color}`} type="submit" {...otherProps} />
-)
-
-export default Button
