5008792c40531cd80488b4756dfdb948bc9a10c8
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-03 19:19:33 -0300

    tests(carFrontPhotoStep.test.js) skip test

--- cypress/e2e/carFrontPhotoStep.test.js
+++ cypress/e2e/carFrontPhotoStep.test.js
@@ -1,15 +1,16 @@
 /// <reference types="cypress" />
 describe("Car front photo upload step", () => {
   beforeEach(() => {
-    cy.visit("/fazer-denuncia")
-    cy.findByText("Enviar foto da frente").as("submitButton")
+    // cy.visit("/fazer-denuncia")
   })
   context("submit button", () => {
     xit("should be disabled when page is loaded", () => {
+      cy.findByText("Enviar foto da frente").as("submitButton")
       cy.get("@submitButton").should("be.disabled")
     })
     xit("should be enabled after file select and disable", () => {
       const fileName = "car-front-example.jpg"
+      cy.findByText("Enviar foto da frente").as("submitButton")
       cy.fixture(fileName).then(fileContent => {
         cy.get("[data-test-id=carFrontImageUploadField]").upload({
           fileContent,
