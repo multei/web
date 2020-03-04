2ec13b070554a07d3b6b1735dd4e84c34adc3fc9
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-04 13:50:17 -0300

    fix(Footer) remove ul > p conflict warning

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,61 +3,58 @@ import { Link as GatsbyLink } from "gatsby"
 
 import Box from "@material-ui/core/Box"
 import Link from "@material-ui/core/Link"
-import Typography from "@material-ui/core/Typography"
 import Hidden from "@material-ui/core/Hidden"
 import HorizontalList from "../HorizontalList"
 import ListItem from "@material-ui/core/ListItem"
-import { Container } from "@material-ui/core"
+import Container from "@material-ui/core/Container"
 
 export default ({ siteTitle }) => {
   return (
     <Hidden smDown>
       <Box component="footer" display="block">
         <Container maxWidth="sm">
-          <Typography align="center" paragraph={true} variant="body2">
-            <HorizontalList>
-              <ListItem>
-                <Link
-                  color="inherit"
-                  component={GatsbyLink}
-                  to="/"
-                  underline="always"
-                >
-                  © {new Date().getFullYear()} {siteTitle}
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link
-                  color="inherit"
-                  component={GatsbyLink}
-                  to="/ajuda"
-                  underline="always"
-                >
-                  Ajuda
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link
-                  color="inherit"
-                  component={GatsbyLink}
-                  to="/privacidade"
-                  underline="always"
-                >
-                  Privacidade
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link
-                  color="inherit"
-                  component={GatsbyLink}
-                  to="/termos"
-                  underline="always"
-                >
-                  Termos de&nbsp;uso
-                </Link>
-              </ListItem>
-            </HorizontalList>
-          </Typography>
+          <HorizontalList>
+            <ListItem>
+              <Link
+                color="inherit"
+                component={GatsbyLink}
+                to="/"
+                underline="always"
+              >
+                © {new Date().getFullYear()} {siteTitle}
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link
+                color="inherit"
+                component={GatsbyLink}
+                to="/ajuda"
+                underline="always"
+              >
+                Ajuda
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link
+                color="inherit"
+                component={GatsbyLink}
+                to="/privacidade"
+                underline="always"
+              >
+                Privacidade
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link
+                color="inherit"
+                component={GatsbyLink}
+                to="/termos"
+                underline="always"
+              >
+                Termos de&nbsp;uso
+              </Link>
+            </ListItem>
+          </HorizontalList>
         </Container>
       </Box>
     </Hidden>
