e402d9c53c10ec85670a96e9704a7a223bd395a5
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-14 12:06:00 -0300

    style(404.js): fix code style

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import React from "react"
 import SEO from "../components/seo"
 import NotFoundTemplate from "../templates/404"
 
-const NotFoundPage = (props) => (
+const NotFoundPage = props => (
   <>
     <SEO title="Página não encontrada" />
     <NotFoundTemplate {...props} />
