e677c93245456e73a8e1377baf230f5487520b8c
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-04-28 19:36:33 -0300

    feat(siteMetadata): add separated config file for site metadata

---
+++ config/siteMetadata/index.js
@@ -0,0 +1,15 @@
+const siteMetadata = {
+  author: "@multeiapp",
+  backgroundColor: "#f6f4f3",
+  description:
+    "Aqui você pode fazer sua denúncia contra estacionamento irregular em vagas de pessoas com deficiência, grávidas e pessoas idosas.",
+  display: "minimal-ui",
+  icon: "src/images/icon.png",
+  shortName: "Multei!",
+  siteUrl: "https://multei.com.br",
+  startUrl: "/",
+  themeColor: "#623CEA",
+  title: "Multei! Denuncie estacionamento irregular",
+}
+
+module.exports = siteMetadata
