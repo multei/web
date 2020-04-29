4d0a2eb6577247aa90026074da4d8536c0ae89ec
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-04-28 22:20:36 -0300

    fix(siteMetadata): add missing lang and titleTemplate metadata

--- config/siteMetadata/index.js
+++ config/siteMetadata/index.js
@@ -3,12 +3,14 @@ const siteMetadata = {
   description:
     "Aqui você pode fazer sua denúncia contra estacionamento irregular em vagas de pessoas com deficiência, grávidas e pessoas idosas.",
   display: "minimal-ui",
+  lang: "pt-br",
   icon: "src/images/icon.png",
   shortName: "Multei!",
   siteUrl: "https://multei.com.br",
   startUrl: "/",
   themeColor: "#623CEA",
   title: "Multei!",
+  titleTemplate: "%s | Multei!",
 }
 
 module.exports = siteMetadata
