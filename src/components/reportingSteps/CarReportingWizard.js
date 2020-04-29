6e24a251b799b97d8c28b3be67c2d79ce907945b
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-04-28 22:21:03 -0300

    fix(CarReportingWizard.js): change back button position on desktop

--- src/components/reportingSteps/CarReportingWizard.js
+++ src/components/reportingSteps/CarReportingWizard.js
@@ -1,16 +1,15 @@
 import React from "react"
 
-import Button from "@material-ui/core/Button"
 import Hidden from "@material-ui/core/Hidden"
 import MobileStepper from "@material-ui/core/MobileStepper"
 import Step from "@material-ui/core/Step"
 import Stepper from "@material-ui/core/Stepper"
 import StepLabel from "@material-ui/core/StepLabel"
 
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft"
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight"
 import Typography from "@material-ui/core/Typography"
 import StepContent from "@material-ui/core/StepContent"
+import BackButton from "../BackButton"
+import NextButton from "../NextButton"
 
 const CarReportingWizard = ({
   activeStep,
@@ -22,19 +21,12 @@ const CarReportingWizard = ({
 }) => {
   const isLastStep = activeStep === maxSteps - 1
   const nextButtonLabel = isLastStep ? "Finalizar" : "Avançar"
-  const backButton = (
-    <Button disabled={activeStep === 0} onClick={onBack}>
-      <KeyboardArrowLeft /> Voltar
-    </Button>
-  )
-  const nextButton = (
-    <Button color="primary" onClick={onNext} variant="contained">
-      {nextButtonLabel} <KeyboardArrowRight />
-    </Button>
-  )
+  const backButton = <BackButton disabled={activeStep === 0} onClick={onBack} />
+  const nextButton = <NextButton onClick={onNext}>{nextButtonLabel}</NextButton>
   return (
     <>
       <Hidden implementation="css" xsDown={true}>
+        {activeStep > 0 && backButton}
         <Stepper activeStep={activeStep} orientation="vertical">
           {steps.map((label, index) => {
             return (
@@ -42,7 +34,6 @@ const CarReportingWizard = ({
                 <StepLabel>{label}</StepLabel>
                 {activeStep === index && (
                   <StepContent>
-                    {activeStep > 0 && backButton}
                     {children}
                     {nextButton}
                   </StepContent>
