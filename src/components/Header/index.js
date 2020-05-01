586a6fd128b779f60101541f9adb9319b6e7913d
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-05-01 19:22:22 -0300

    fix(Header): try to fix overflow on header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,17 +6,33 @@ import Toolbar from "@material-ui/core/Toolbar"
 
 import { PageHeader, SiteTitle } from "muy"
 import useStandalone from "../../hooks/useStandalone"
+import makeStyles from "@material-ui/core/styles/makeStyles"
 
-const Header = ({ siteTitle }) => (
-  <AppBar
-    component={PageHeader}
-    position={useStandalone() ? "fixed" : "static"}
-  >
-    <Toolbar>
-      <SiteTitle>{siteTitle}</SiteTitle>
-    </Toolbar>
-  </AppBar>
-)
+const pixels = 1
+const statusBarHeight = 20 * pixels
+
+const useStyles = makeStyles({
+  root: {
+    paddingTop: `${statusBarHeight}px`,
+    top: `${statusBarHeight * -1}px`,
+  },
+})
+
+const Header = ({ siteTitle }) => {
+  const standalone = useStandalone()
+  const classes = useStyles()
+  return (
+    <AppBar
+      classes={standalone && classes}
+      component={PageHeader}
+      position={standalone ? "fixed" : "static"}
+    >
+      <Toolbar>
+        <SiteTitle>{siteTitle}</SiteTitle>
+      </Toolbar>
+    </AppBar>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
