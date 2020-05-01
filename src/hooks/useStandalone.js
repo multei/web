546e9b8954f343ad0a26e4ede6b757f0374e573f
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-05-01 15:47:45 -0300

    fix(useStandalone): detect window and navigator

--- src/hooks/useStandalone.js
+++ src/hooks/useStandalone.js
@@ -4,10 +4,13 @@ const useStandalone = () => {
   if (useMediaQuery("(display-mode: standalone)")) {
     return true
   }
-  if (typeof window.navigator === "undefined") {
+  if (
+    typeof window === "undefined" ||
+    typeof window.navigator === "undefined"
+  ) {
     return false
   }
-  return Boolean(navigator.standalone)
+  return Boolean(window.navigator.standalone)
 }
 
 export default useStandalone
