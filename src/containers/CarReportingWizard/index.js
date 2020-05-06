4ea56a92a66b3ddf68268887346165e071c8a93d
Leandro Miglioli <lmiglio@thoughtworks.com>
2020-05-06 15:07:20 -0300

    feat(CarReportingWizard): add support to feature toggle

--- src/containers/CarReportingWizard/index.js
+++ src/containers/CarReportingWizard/index.js
@@ -5,37 +5,58 @@ import CarPlateConfirmStep from "../../containers/CarPlateConfirmStep"
 import LocationStep from "../../containers/LocationStep"
 import SuccessStep from "../../components/SuccessStep"
 import useGlobal from "../../hooks/useGlobal"
+import useFeatureToggle from "../../hooks/useFeatureToggle"
 
-function getSteps() {
-  return [
+function getSteps(getPlateConfirmationStepToggle) {
+  return getPlateConfirmationStepToggle ? [
     <>Enviar foto da&nbsp;frente</>,
     <>Confirmar placa</>,
     <>Enviar localização</>,
     <>Denúncia realizada</>,
+  ] : 
+  [
+    <>Enviar foto da&nbsp;frente</>,
+    <>Enviar localização</>,
+    <>Denúncia realizada</>,
   ]
 }
+
 
 export default () => {
   const [activeStep, setActiveStep] = useState(0)
   const [globalState] = useGlobal()
   const [transitionDirection, setTransitionDirection] = useState("left")
 
-  const steps = getSteps()
+  const [getPlateConfirmationStepToggle] = useFeatureToggle("PLATE_CONFIRMATION_STEP")
+  
+  const steps = getSteps(getPlateConfirmationStepToggle)
   const maxSteps = steps.length
 
   const getStepContent = (stepIndex) => {
-    switch (stepIndex) {
-      case 0:
-        return <CarFrontPhotoStep />
-      case 1:
-        return <CarPlateConfirmStep />
-      case 2:
-        return <LocationStep />
-      case 3:
-        return <SuccessStep />
-      default:
-        throw new Error("Invalid step index")
-    }
+    if(getPlateConfirmationStepToggle)
+      switch (stepIndex) {
+        case 0:
+          return <CarFrontPhotoStep />
+        case 1:
+          return <CarPlateConfirmStep />
+        case 2:
+          return <LocationStep />
+        case 3:
+          return <SuccessStep />
+        default:
+          throw new Error("Invalid step index")
+      }
+    else
+      switch (stepIndex) {
+        case 0:
+          return <CarFrontPhotoStep />
+        case 1:
+          return <LocationStep />
+        case 2:
+          return <SuccessStep />
+        default:
+          throw new Error("Invalid step index")
+      }
   }
 
   const handleNext = () => {
@@ -53,19 +74,30 @@ export default () => {
   }
 
   const nextButtonDisabled = () => {
-    switch (activeStep) {
-      case 0:
-        return !globalState.currentParkingReportingData.carFrontPhotoPreviewUrl
-      case 1:
-        return !(globalState.currentParkingReportingData.carPlate.length === 7)
-      case 2:
-        return !globalState.currentPosition
-      default:
-        return false
-    }
+    if(getPlateConfirmationStepToggle)
+      switch (activeStep) {
+        case 0:
+          return !globalState.currentParkingReportingData.carFrontPhotoPreviewUrl
+        case 1:
+          return !(globalState.currentParkingReportingData.carPlate.length === 7)
+        case 2:
+          return !globalState.currentPosition
+        default:
+          return false
+      }
+    else
+      switch (activeStep) {
+        case 0:
+          return !globalState.currentParkingReportingData.carFrontPhotoPreviewUrl
+        case 1:
+          return !globalState.currentPosition
+        default:
+          return false
+      }
   }
-
-  const nextButtonLabel = activeStep === 2 ? "Finalizar" : "Avançar"
+  
+  const lastStep = maxSteps - 1
+  const nextButtonLabel = activeStep === lastStep - 1 ? "Finalizar" : "Avançar"
 
   return (
     <CarReportingWizard
@@ -76,9 +108,9 @@ export default () => {
       maxSteps={maxSteps}
       nextButtonDisabled={nextButtonDisabled()}
       nextButtonLabel={nextButtonLabel}
-      showBackButton={activeStep > 0 && activeStep < 3}
-      showMobileStepper={activeStep < 3}
-      showNextButton={activeStep < 3}
+      showBackButton={activeStep > 0 && activeStep < lastStep}
+      showMobileStepper={activeStep < lastStep}
+      showNextButton={activeStep < lastStep}
       transitionDirection={transitionDirection}
       steps={steps}
     >
