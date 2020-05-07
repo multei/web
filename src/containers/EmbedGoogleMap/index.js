f61a9807e64ca34405cd23ad4df4c976964d8512
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-05-07 19:32:07 -0300

    fix(EmbedGoogleMap): remove uneeded space

--- src/containers/EmbedGoogleMap/index.js
+++ src/containers/EmbedGoogleMap/index.js
@@ -3,7 +3,7 @@ import { EmbedGoogleMap as MuyEmbedGoogleMap } from "muy"
 
 const EmbedGoogleMap = ({ coordinates, ...props }) => (
   <MuyEmbedGoogleMap
-    apiKey={process.env. GATSBY_GOOGLE_MAPS_EMBED_API_KEY}
+    apiKey={process.env.GATSBY_GOOGLE_MAPS_EMBED_API_KEY}
     coordinates={coordinates}
     {...props}
   />
