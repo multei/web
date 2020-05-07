42ae1834bf1aef15ac928db8a281926ca9bff9e0
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-05-07 19:59:16 -0300

    test(cypress): refactor e2e tests

---
+++ cypress/e2e/index.spec.js
@@ -0,0 +1,46 @@
+/// <reference types="cypress" />
+/// <reference types="cypress-axe" />
+
+describe("Home page", () => {
+  before(() => {
+    cy.visit("/")
+  })
+  it("should have no detectable accessibility violations on load", () => {
+    cy.injectAxe()
+    cy.wait(500)
+    cy.checkA11y()
+  })
+  it("should have canonical URL on meta", () => {
+    cy.get("head link[rel=canonical]").should(
+      "have.attr",
+      "href",
+      "https://multei.com.br/"
+    )
+  })
+  it("should have site header present on header", () => {
+    cy.visit("/")
+    cy.get('header[role="banner"] h1 a').contains("Multei!")
+  })
+})
+
+describe("Parking report page", () => {
+  before(() => {
+    cy.visit("/denunciar")
+  })
+  it("should have no detectable accessibility violations on load", () => {
+    cy.injectAxe()
+    cy.wait(500)
+    cy.checkA11y()
+  })
+  it("should have canonical URL on meta", () => {
+    cy.get("head link[rel=canonical]").should(
+      "have.attr",
+      "href",
+      "https://multei.com.br/denunciar/"
+    )
+  })
+  it("should have site header present on header", () => {
+    cy.visit("/")
+    cy.get('header[role="banner"] h1 a').contains("Multei!")
+  })
+})
--- cypress/e2e/siteTitle.spec.js
+++
@@ -1,6 +0,0 @@
-describe("Site title", () => {
-  it("should be present on header", () => {
-    cy.visit("/")
-    cy.get('header[role="banner"] h1 a').contains("Multei!")
-  })
-})
