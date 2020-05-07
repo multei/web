cabb6b2b48a93c0fabfb19dcc7c0d1af04fb3a13
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-05-07 19:30:35 -0300

    style(CarReportingWizard): prettify

--- src/containers/CarReportingWizard/index.js
+++ src/containers/CarReportingWizard/index.js
@@ -8,32 +8,34 @@ import useGlobal from "../../hooks/useGlobal"
 import useFeatureToggle from "../../hooks/useFeatureToggle"
 
 function getSteps(getPlateConfirmationStepToggle) {
-  return getPlateConfirmationStepToggle ? [
-    <>Enviar foto da&nbsp;frente</>,
-    <>Confirmar placa</>,
-    <>Enviar localização</>,
-    <>Denúncia realizada</>,
-  ] : 
-  [
-    <>Enviar foto da&nbsp;frente</>,
-    <>Enviar localização</>,
-    <>Denúncia realizada</>,
-  ]
+  return getPlateConfirmationStepToggle
+    ? [
+        <>Enviar foto da&nbsp;frente</>,
+        <>Confirmar placa</>,
+        <>Enviar localização</>,
+        <>Denúncia realizada</>,
+      ]
+    : [
+        <>Enviar foto da&nbsp;frente</>,
+        <>Enviar localização</>,
+        <>Denúncia realizada</>,
+      ]
 }
-
 
 export default () => {
   const [activeStep, setActiveStep] = useState(0)
   const [globalState] = useGlobal()
   const [transitionDirection, setTransitionDirection] = useState("left")
 
-  const [getPlateConfirmationStepToggle] = useFeatureToggle("PLATE_CONFIRMATION_STEP")
-  
+  const [getPlateConfirmationStepToggle] = useFeatureToggle(
+    "PLATE_CONFIRMATION_STEP"
+  )
+
   const steps = getSteps(getPlateConfirmationStepToggle)
   const maxSteps = steps.length
 
   const getStepContent = (stepIndex) => {
-    if(getPlateConfirmationStepToggle)
+    if (getPlateConfirmationStepToggle)
       switch (stepIndex) {
         case 0:
           return <CarFrontPhotoStep />
@@ -74,12 +76,15 @@ export default () => {
   }
 
   const nextButtonDisabled = () => {
-    if(getPlateConfirmationStepToggle)
+    if (getPlateConfirmationStepToggle)
       switch (activeStep) {
         case 0:
-          return !globalState.currentParkingReportingData.carFrontPhotoPreviewUrl
+          return !globalState.currentParkingReportingData
+            .carFrontPhotoPreviewUrl
         case 1:
-          return !(globalState.currentParkingReportingData.carPlate.length === 7)
+          return !(
+            globalState.currentParkingReportingData.carPlate.length === 7
+          )
         case 2:
           return !globalState.currentPosition
         default:
@@ -88,14 +93,15 @@ export default () => {
     else
       switch (activeStep) {
         case 0:
-          return !globalState.currentParkingReportingData.carFrontPhotoPreviewUrl
+          return !globalState.currentParkingReportingData
+            .carFrontPhotoPreviewUrl
         case 1:
           return !globalState.currentPosition
         default:
           return false
       }
   }
-  
+
   const lastStep = maxSteps - 1
   const nextButtonLabel = activeStep === lastStep - 1 ? "Finalizar" : "Avançar"
 
