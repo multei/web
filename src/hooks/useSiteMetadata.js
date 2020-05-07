aa4622a6dbc77bfa2b1a3d6001e79fd6117709e2
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-05-07 20:00:14 -0300

    feat(useSiteMetadata): add googleSiteVerification field

--- src/hooks/useSiteMetadata.js
+++ src/hooks/useSiteMetadata.js
@@ -8,6 +8,7 @@ const useSiteMetadata = () => {
           applicationName
           description
           lang
+          googleSiteVerification
           statusBarStyle
           themeColor
           title
