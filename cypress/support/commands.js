fb60d2c6b38e178f05af21687869c250a256a426
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-05-16 13:18:30 -0300

    test(setFeatureToggle): close #191 creating Cypress command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,2 +1,10 @@
 import "cypress-file-upload"
 import "@testing-library/cypress/add-commands"
+
+const setFeatureToggle = (key, value) => {
+  cy.log(`Setting ${key} toggle to ${value}`)
+  Cypress.env(`GATSBY_TOGGLE_${key}`, value)
+  localStorage.setItem(`TOGGLE_${key}`, value)
+}
+
+Cypress.Commands.add("setFeatureToggle", setFeatureToggle)
