e318b6b930d674e27bc598040c0934261317eed9
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-05-16 13:54:02 -0300

    test(routePageDataRequests): add more routes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -13,12 +13,32 @@ const routePageDataRequests = () => {
   cy.server()
   cy.route({
     method: "GET",
+    url: "/socket.io/**",
+  }).as("socketIoGet")
+  cy.route({
+    method: "POST",
+    url: "/socket.io/**",
+  }).as("socketIoPost")
+  cy.route({
+    method: "GET",
     url: "/page-data/app-data.json",
   }).as("appData")
   cy.route({
     method: "GET",
     url: "/page-data/index/page-data.json",
   }).as("indexPageData")
+  cy.route({
+    method: "GET",
+    url: "/page-data/dev-404-page/page-data.json",
+  }).as("notFoundDevPageData")
+  cy.route({
+    method: "GET",
+    url: "/page-data/404.html/page-data.json",
+  }).as("notFoundPageData")
+  cy.route({
+    method: "GET",
+    url: "/page-data/denunciar/page-data.json",
+  }).as("reportPageData")
 }
 
 Cypress.Commands.add("routePageDataRequests", routePageDataRequests)
