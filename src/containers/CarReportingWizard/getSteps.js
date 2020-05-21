a05b53b13bd99c58f80c06ef83654ba7c4562904
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-05-21 13:42:45 -0300

    style(CarReportingWizard): after prettier formatting

--- src/containers/CarReportingWizard/getSteps.js
+++ src/containers/CarReportingWizard/getSteps.js
@@ -1,14 +1,15 @@
 import React from "react"
 
-export const getSteps = ({ getPlateConfirmationStepToggle, carReportPhotoInstructions }) => {
-  let steps = [];
+export const getSteps = ({
+  getPlateConfirmationStepToggle,
+  carReportPhotoInstructions,
+}) => {
+  let steps = []
   steps.push(<>Enviar foto da&nbsp;frente</>)
 
-  if (getPlateConfirmationStepToggle)
-    steps.push(<>Confirmar placa</>)
+  if (getPlateConfirmationStepToggle) steps.push(<>Confirmar placa</>)
 
-  if (carReportPhotoInstructions)
-    steps.push(<>Instruções</>)
+  if (carReportPhotoInstructions) steps.push(<>Instruções</>)
 
   steps.push(<>Enviar localização</>)
   steps.push(<>Denúncia realizada</>)
--- src/containers/CarReportingWizard/getSteps.test.js
+++ src/containers/CarReportingWizard/getSteps.test.js
@@ -2,38 +2,37 @@ import React from "react"
 import { getSteps } from "./getSteps"
 
 describe("<getSteps />", () => {
+  it("should return all steps", async () => {
+    const numberOfSteps = 5
 
-    it("should return all steps", async () => {
-        const numberOfSteps = 5
+    const featureToggle = {
+      getPlateConfirmationStepToggle: true,
+      carReportPhotoInstructions: true,
+    }
 
-        const featureToggle = {
-            getPlateConfirmationStepToggle: true,
-            carReportPhotoInstructions: true,
-        };
+    const [steps, maxSteps] = getSteps(featureToggle)
 
-        const [steps, maxSteps] = getSteps(featureToggle);
+    expect(steps.length).toBe(numberOfSteps)
+    expect(maxSteps).toBe(numberOfSteps)
+  })
 
-        expect(steps.length).toBe(numberOfSteps);
-        expect(maxSteps).toBe(numberOfSteps);
-    })
+  it("should not return confirm plate step", async () => {
+    const featureToggle = {
+      getPlateConfirmationStepToggle: false,
+    }
 
-    it("should not return confirm plate step", async () => {
-        const featureToggle = {
-            getPlateConfirmationStepToggle: false,
-        };
+    const [steps, maxSteps] = getSteps(featureToggle)
 
-        const [steps, maxSteps] = getSteps(featureToggle);
+    expect(steps.includes(<>Confirmar placa</>)).toBeFalsy()
+  })
 
-        expect(steps.includes(<>Confirmar placa</>)).toBeFalsy()
-    })
+  it("should not return car report photo instructions", async () => {
+    const featureToggle = {
+      carReportPhotoInstructions: false,
+    }
 
-    it("should not return car report photo instructions", async () => {
-          const featureToggle = {
-          carReportPhotoInstructions: false,
-        };
+    const [steps, maxSteps] = getSteps(featureToggle)
 
-        const [steps, maxSteps] = getSteps(featureToggle);
-
-        expect(steps.includes(<>Instruções</>)).toBeFalsy()
-    })
+    expect(steps.includes(<>Instruções</>)).toBeFalsy()
+  })
 })
