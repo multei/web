9baaf3a5955dab215fb3bdf391b7c92590e6957c
Heitor Inhaquites and Jimmy Andrade and Túlio Assis <heitor.inhaquites+jimmy.andrade+tulio.assis@thoughtworks.com>
2020-05-22 15:22:15 -0300

    test(useReportingWizardStep): add first test

---
+++ src/hooks/useReportingWizardSteps/useReportingWizardSteps.test.js
@@ -0,0 +1,8 @@
+import { useReportingWizardSteps } from "./useReportingWizardSteps"
+
+describe("Reporting wizard steps", () => {
+  xit("should", () => {
+    const steps = useReportingWizardSteps()
+    expect(steps).toBeInstanceOf(Object)
+  })
+})
