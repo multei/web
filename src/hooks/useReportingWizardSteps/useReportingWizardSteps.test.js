996bc6df8b072314ae52ac590f85b233f0ef9e49
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-05-22 15:35:32 -0300

    fix(useReportingWizardSteps): make hook return array

--- src/hooks/useReportingWizardSteps/useReportingWizardSteps.test.js
+++ src/hooks/useReportingWizardSteps/useReportingWizardSteps.test.js
@@ -5,4 +5,14 @@ describe("Reporting wizard steps", () => {
     const steps = useReportingWizardSteps()
     expect(steps).toBeInstanceOf(Array)
   })
+
+  xit("should return array with at least one object", () => {    
+    const steps = useReportingWizardSteps()
+    expect(steps.length > 0).toBeTrue()
+  })
+
+  xit("should return an array of objects with label and render properties", () => {
+    const steps = useReportingWizardSteps()
+    expect(steps.every(step => step.label !== undefined && step.render !== undefined)).toBeTrue()
+  })
 })
