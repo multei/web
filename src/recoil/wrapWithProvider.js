a612f986487b8ee5488dac1776a0654d10a8cde0
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-06-03 17:48:02 -0300

    refactor(wrapWithProvider): remove unused file

--- src/recoil/wrapWithProvider.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { RecoilRoot } from "recoil"
-
-// eslint-disable-next-line react/display-name,react/prop-types
-export const wrapWithProvider = ({ element }) => (
-  <RecoilRoot>{element}</RecoilRoot>
-)
