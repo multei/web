ec443e67bf9ca09e996e44de5a681b7b4c167cab
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-06-03 17:53:47 -0300

    feat(CopyrightText): add new container

---
+++ src/containers/CopyrightText/index.test.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+/**
+ * @todo Implement tests for Copyright text container
+ */
+describe("Copyright text container", () => {
+  it.todo("should render correctly with mocked site title")
+})
