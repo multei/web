172fdc1009001a69a8ab782116eda094b36fa5fc
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-06-03 17:58:49 -0300

    feat(NavigationDrawer): add component

---
+++ src/components/NavigationDrawer/index.test.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+/**
+ * @todo Create tests for <NavigationDrawer /> varying by feature toggle value
+ */
+describe("<NavigationDrawer /> component", () => {
+  it.todo("should render correctly with related feature toggle disabled")
+  it.todo("should render correctly with related feature toggle enabled")
+})
