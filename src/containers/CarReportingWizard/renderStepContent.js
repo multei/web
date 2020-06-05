9129d105d0fc7bb56d023acff8491fc79b362b17
Túlio Assis <tulioassis@outlook.com>
2020-06-05 15:36:40 -0300

    style(useReportingWizardSteps): prettier format

--- src/containers/CarReportingWizard/renderStepContent.js
+++ src/containers/CarReportingWizard/renderStepContent.js
@@ -1,9 +1,6 @@
 import React from "react"
 
-export const renderStepContent = ({
-  steps,
-  stepIndex,
-}) => {
+export const renderStepContent = ({ steps, stepIndex }) => {
   const step = steps[stepIndex]
   const Component = step.component
   return <Component />
