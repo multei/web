4245eaa15953ffc42723d7ea2a2b3543e8e143f2
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-06-10 11:00:51 -0300

    chore(CarPhotoInstructionsStep): include instruction message

--- src/components/CarPhotoInstructionsStep/CarPhotoInstructionsStep.js
+++ src/components/CarPhotoInstructionsStep/CarPhotoInstructionsStep.js
@@ -1,5 +1,10 @@
 import React from "react"
+import { Paragraph } from "muy"
 
 export const CarPhotoInstructionsStep = () => (
-  <div>CarPhotoInstructionsStep</div>
+  <div>
+    <Paragraph variant="body2">
+      A foto deve mostrar o veículo por inteiro, deixando visível a placa e o painel do veículo, provando que não há credencial devidamente exposta. Você pode enquadrar evidências de que a vaga é reservada a pessoas com deficiência para facilitar o processo de denúncia.
+    </Paragraph>
+  </div>
 )
---
+++ src/components/CarPhotoInstructionsStep/CarPhotoInstructionsStep.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { CarPhotoInstructionsStep } from './CarPhotoInstructionsStep'
+
+describe('CarPhotoInstructionsStep tests', () => {
+  it('should render instruction message', () => {
+    render(< CarPhotoInstructionsStep />);
+    const element = screen.getByText(`A foto deve mostrar o veículo por inteiro, deixando visível a placa e o painel do veículo, provando que não há credencial devidamente exposta. Você pode enquadrar evidências de que a vaga é reservada a pessoas com deficiência para facilitar o processo de denúncia.`)
+    expect(element).toBeTruthy()
+  })
+})
