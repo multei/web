f73478ec2aaa6c6106c5bf98bcee781284f23962
Túlio Assis <tulioassis@outlook.com>
2020-06-15 16:44:42 -0300

    chore(CarPhotoInstructionsStep): add car photo example

--- src/components/CarPhotoInstructionsStep/CarPhotoInstructionsStep.js
+++ src/components/CarPhotoInstructionsStep/CarPhotoInstructionsStep.js
@@ -1,10 +1,15 @@
 import React from "react"
-import { Paragraph } from "muy"
+import { Paragraph, Image } from "muy"
+import carPhotoModel from "../../images/car_model.png"
 
 export const CarPhotoInstructionsStep = () => (
   <div>
     <Paragraph variant="body2">
       A foto deve mostrar o veículo por inteiro, deixando visível a placa e o painel do veículo, provando que não há credencial devidamente exposta. Você pode enquadrar evidências de que a vaga é reservada a pessoas com deficiência para facilitar o processo de denúncia.
     </Paragraph>
+    <Image
+      alt="Foto de exemplo do carro"
+      src={carPhotoModel}
+    />
   </div>
 )
--- src/components/CarPhotoInstructionsStep/CarPhotoInstructionsStep.test.js
+++ src/components/CarPhotoInstructionsStep/CarPhotoInstructionsStep.test.js
@@ -8,4 +8,10 @@ describe('CarPhotoInstructionsStep tests', () => {
     const element = screen.getByText(`A foto deve mostrar o veículo por inteiro, deixando visível a placa e o painel do veículo, provando que não há credencial devidamente exposta. Você pode enquadrar evidências de que a vaga é reservada a pessoas com deficiência para facilitar o processo de denúncia.`)
     expect(element).toBeTruthy()
   })
+
+  it('should render instruction image', () => {
+    render(< CarPhotoInstructionsStep />);
+    const element = screen.getByAltText(`Foto de exemplo do carro`)
+    expect(element).toBeTruthy()
+  })
 })
