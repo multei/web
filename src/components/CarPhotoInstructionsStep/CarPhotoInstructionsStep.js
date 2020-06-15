0fe8d17a6503ee48692ab0c9cd73261bc6bc8d87
Túlio Assis <tulioassis@outlook.com>
2020-06-15 17:09:55 -0300

    style(CarPhotoInstructionsStep): prettier format

--- src/components/CarPhotoInstructionsStep/CarPhotoInstructionsStep.js
+++ src/components/CarPhotoInstructionsStep/CarPhotoInstructionsStep.js
@@ -5,11 +5,11 @@ import carPhotoModel from "../../images/car_model.png"
 export const CarPhotoInstructionsStep = () => (
   <div>
     <Paragraph variant="body2">
-      A foto deve mostrar o veículo por inteiro, deixando visível a placa e o painel do veículo, provando que não há credencial devidamente exposta. Você pode enquadrar evidências de que a vaga é reservada a pessoas com deficiência para facilitar o processo de denúncia.
+      A foto deve mostrar o veículo por inteiro, deixando visível a placa e o
+      painel do veículo, provando que não há credencial devidamente exposta.
+      Você pode enquadrar evidências de que a vaga é reservada a pessoas com
+      deficiência para facilitar o processo de denúncia.
     </Paragraph>
-    <Image
-      alt="Foto de exemplo do carro"
-      src={carPhotoModel}
-    />
+    <Image alt="Foto de exemplo do carro" src={carPhotoModel} />
   </div>
 )
--- src/components/CarPhotoInstructionsStep/CarPhotoInstructionsStep.test.js
+++ src/components/CarPhotoInstructionsStep/CarPhotoInstructionsStep.test.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import { CarPhotoInstructionsStep } from './CarPhotoInstructionsStep'
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import { CarPhotoInstructionsStep } from "./CarPhotoInstructionsStep"
 
-describe('CarPhotoInstructionsStep tests', () => {
-  it('should render instruction message', () => {
-    render(< CarPhotoInstructionsStep />);
-    const element = screen.getByText(`A foto deve mostrar o veículo por inteiro, deixando visível a placa e o painel do veículo, provando que não há credencial devidamente exposta. Você pode enquadrar evidências de que a vaga é reservada a pessoas com deficiência para facilitar o processo de denúncia.`)
+describe("CarPhotoInstructionsStep tests", () => {
+  it("should render instruction message", () => {
+    render(<CarPhotoInstructionsStep />)
+    const element = screen.getByText(
+      `A foto deve mostrar o veículo por inteiro, deixando visível a placa e o painel do veículo, provando que não há credencial devidamente exposta. Você pode enquadrar evidências de que a vaga é reservada a pessoas com deficiência para facilitar o processo de denúncia.`
+    )
     expect(element).toBeTruthy()
   })
 
-  it('should render instruction image', () => {
-    render(< CarPhotoInstructionsStep />);
+  it("should render instruction image", () => {
+    render(<CarPhotoInstructionsStep />)
     const element = screen.getByAltText(`Foto de exemplo do carro`)
     expect(element).toBeTruthy()
   })
