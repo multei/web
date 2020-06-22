463adbcc2e5a79e2fb48f0492a441b42acb1998a
Túlio Assis <tulioassis@outlook.com>
2020-06-22 18:50:38 -0300

    chore(services/parking): add createParkingReport service

--- src/services/parkings.js
+++ src/services/parkings.js
@@ -2,3 +2,12 @@ import Api from "../api"
 
 export const getParkingsByCarPlate = (carPlate, version = 1) =>
   Api().get(`/v${version}/parkings/${carPlate}`)
+
+export const createParkingReport = (parkingReportState, version = 1) => {
+  const carFrontPhoto = new File([parkingReportState['carFrontPhotoPreviewUrl']], 'photo.png');
+
+  const formData = new FormData();
+  formData.append("car_front_photo", carFrontPhoto);
+
+  return Api().post(`/v${version}/parkings/`, formData)
+}
---
+++ src/services/parkings.test.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+import Api from "../api"
+import { getParkingsByCarPlate, createParkingReport } from "./parkings"
+
+jest.mock("../api")
+
+describe("Parkings API", () => {
+  beforeEach(() => {
+    Api.mockReturnValue({
+      get: jest.fn(),
+      post: jest.fn(),
+    })
+  })
+
+  it("should call parkings API get with car plate", () => {
+    const carPlate = "ABC1234"
+    const version = 0
+
+    getParkingsByCarPlate(carPlate, version)
+
+    expect(Api().get).toHaveBeenCalledWith(`/v${version}/parkings/${carPlate}`)
+  })
+
+  it("should call parkings API post with car photo", () => {
+    const carFrontPhotoPreviewUrl = 'photo-url'
+    const parkingReportState = {
+      'carFrontPhotoPreviewUrl': carFrontPhotoPreviewUrl
+    }
+    const version = 0
+    
+    const formData = new FormData();
+    formData.append("car_front_photo", carFrontPhotoPreviewUrl);
+
+    createParkingReport(parkingReportState, version)
+
+    expect(Api().post).toHaveBeenCalledWith(`/v${version}/parkings/`, expect.objectContaining(formData))
+  })
+
+})
