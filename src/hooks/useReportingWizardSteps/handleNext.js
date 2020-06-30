619e6cff2b7fc77777eb2e79241bff40557ea0b5
Túlio Assis <tulioassis@outlook.com>
2020-06-30 13:16:47 -0300

    fix(handleNext): remove deleteItem when it get an error to create parking report

--- src/hooks/useReportingWizardSteps/handleNext.js
+++ src/hooks/useReportingWizardSteps/handleNext.js
@@ -21,8 +21,6 @@ export const handleCarFrontPhoto = (parkingReportState) => {
   }
 
   const handleError = (error) => {
-    localStorage.removeItem(`PARKING_REPORT`)
-
     return {
       success: false,
       newParkingReportState: {
